90ee052884c12b40d36416c8c4a984bf9b7d63ee
Martin Vogwell <martin.vogwell@ultra-electronics.com>
2022-11-24 17:11:05 +0000

    Updated for v1.2 - Changed to export collated single file unless -ExportIndividualLogs switch is used. Changed requirement to always run as admin but now have alert to inform users that they'll get more info by running elevated.

--- Readme.md
+++ Readme.md
@@ -1,26 +1,78 @@
 # ps_Util_GetEventLogByDateRange.ps1
 
-This script will search through all available Windows eventlogs and return all entries found for a specific date range. Results will be exported to a folder with the filename set as the eventlog name.
+* A PowerShell script to export all available Windows event logs for a specific date/time range to a csv type file.<br><br>
+* The script exports (by default) to a single file, collating log data in a csv format which can be opened in a spreadsheet application.<br><br>
+* The script has an option to export each windows log to a separate log file.<br><br>
+* The csv delimeter used is the ~ character.<br><br>
+* Line breaks are removed from the event log messages and replace with " :: ".<br><br>
+* By default the Security event log is not exported. 
+* The startup parameters of the script provide options to include the Security log, set the start and end date/times and export separate csv files for each windows log.<br><br>
+* Whilst the script can be run without admin rights - some event logs will not be exported (including the security event log) without running the script with elevated privileges. <br><br>
 
 ## Parameters
 
-PARAMETER ExportFolderPath
-    [Optional]  This is the path to save the results to. If not specified then c:\temp\EventExport will be used.
+* PARAMETER ExportFolderPath
+  * [Optional]  This is the path to save the results to. If not specified then c:\temp\EventExport will be used (if this folder doesn't exist it will be created). <br><br>
 
-PARAMETER StartDate
-	[Optional] If empty the EndDate will be set to the current date/time minus 24 hours. If specified the date/time must be in the format dd/mm/yyyy hh:mm:ss
+* PARAMETER StartDate
+  * [Optional] If empty the EndDate will be set to the current date/time minus 24 hours. If the parameter is used, the date/time must be in the format dd/mm/yyyy hh:mm:ss (or the local date format).<br><br>
 	
-PARAMETER EndDate
-	[Optional] If empty the EndDate will be set to the current date/time. If specified the date/time must be in the format dd/mm/yyyy hh:mm:ss
+* PARAMETER EndDate
+  * [Optional] If empty the EndDate will be set to the current date/time. If the parameter is used, the date/time must be in the format dd/mm/yyyy hh:mm:ss (or the local date format).<br><br>
+
+* PARAMETER IncludeSecurityLog
+  * [Optional] By default the security log is not exported (as it can take a while). Using this switch includes the security log data in results file.<br><br>
+
+* PARAMETER ExportIndividualLogs
+  * [Optional] By default all log entries are collated into a single file in the export folder. Using this switch will export each windows log into a separate file.<br><br>
+
+<br>
 
 ## Examples
 
-EXAMPLE
+### EXAMPLE - Default options
 
-.\ps_Util_GetEventLogByDateRange.ps1 -ExportFolderPath c:\MyFolderExport
+`.\ps_Util_GetEventLogByDateRange.ps1`
 
-Uses the default path	
-	
-EXAMPLE
+* Exports all windows log data available for the last 24 hours, except the Security Log
+  * The security log is excluded by default as it can take a longer time to export
+* Exports collated data to the default path c:\temp\EventExport
 
-.\ps_Util_GetEventLogByDateRange.ps1 -ExportFolderPath c:\MyFolderExport
+<br>
+
+### EXAMPLE -Include the security log
+
+`.\ps_Util_GetEventLogByDateRange.ps1 -IncludeSecurityLog`
+
+* Exports all windows log data available for the last 24 hours, including the Security Log
+* Exports collated data to the default path c:\temp\EventExport
+
+<br>
+
+### EXAMPLE - Set a date/time range to capture
+
+`.\ps_Util_GetEventLogByDateRange.ps1 -StartDate "01/01/2022 10:00" -EndDate "01/01/2022 11:00"`
+
+* Exports windows log data available for a specific time period - in this case between 10am and 11am on the 1st of Jan 2022
+* Exports collated data to the default path c:\temp\EventExport
+
+<br>
+
+### EXAMPLE -
+
+`.\ps_Util_GetEventLogByDateRange.ps1 -ExportIndividualLogs`
+
+* Exports all windows log data available for the last 24 hours, excluding the Security Log
+* Exports a collated data to the default path c:\temp\EventExport
+* Exports each log to an individual csv file as well creating a collated log file
+
+<br>
+
+### EXAMPLE - Export to a custom folder
+
+`.\ps_Util_GetEventLogByDateRange.ps1 -ExportFolderPath "c:\MyExportFolder"`
+
+* Exports all windows log data available for the last 24 hours, excluding the Security Log
+* Exports collated data to a custom path specified in "ExportFolderPath" (in this instance c:\MyExportFolder)
+
+
--- ps_Util_GetWinEventLogByDateRange.ps1
+++ ps_Util_GetWinEventLogByDateRange.ps1
@@ -1,51 +1,54 @@
 <#
+	.SYNOPSIS
+	This script will search through all available Windows eventlogs and return all entries found for a specific date range. Results will be exported to a folder with the filename set as the eventlog name.
 
-.SYNOPSIS
-This script will search through all available Windows eventlogs and return all entries found for a specific date range. Results will be exported to a folder with the filename set as the eventlog name.
-
-.PARAMETER ExportFolderPath
+	.PARAMETER ExportFolderPath
     [Optional]  This is the path to save the results to. If not specified then c:\temp\EventExport will be used.
 
-.PARAMETER StartDate
+	.PARAMETER StartDate
 	[Optional] If empty the EndDate will be set to the current date/time minus 24 hours. If specified the date/time must be in the format "dd/mm/yyyy hh:mm:ss" and should be specified as a string (not datetime)
 
-.PARAMETER EndDate
+	.PARAMETER EndDate
 	[Optional] If empty the EndDate will be set to the current date/time. If specified the date/time must be in the format "dd/mm/yyyy hh:mm:ss" and should be specified as a string (not datetime)
 
-.PARAMETER ScanSecurityLog
+	.PARAMETER IncludeSecurityLog
 	[Optional] If set to true the security log will be included in the results. By default the security scan is NOT included
 
-.EXAMPLE
+	.PARAMETER ExportIndividualLogs
+	[Optional] To export each individual log to a separate file start the script with this switch
 
-.\ps_Util_GetEventLogByDateRange.ps1
+	.EXAMPLE
 
-Uses the default path
+	.\ps_Util_GetEventLogByDateRange.ps1
 
-.EXAMPLE
+	Uses the default path
 
-.\ps_Util_GetEventLogByDateRange.ps1 -ExportFolderPath c:\MyFolderExport
+	.EXAMPLE
 
-Export the results to c:\MyFolderExport
+	.\ps_Util_GetEventLogByDateRange.ps1 -ExportFolderPath c:\MyFolderExport
 
-.EXAMPLE
+	Export the results to c:\MyFolderExport
 
-.\ps_Util_GetEventLogByDateRange.ps1 -ScanSecurityLog:$True
+	.EXAMPLE
 
-The security log will be included in the results
+	.\ps_Util_GetEventLogByDateRange.ps1 -IncludeSecurityLog
 
-.EXAMPLE
+	The security log will be included in the results
 
-.\ps_Util_GetEventLogByDateRange.ps1 -StartDate "01/01/2019 08:00:00" -EndDate "01/01/2019 09:00:00"
+	.EXAMPLE
 
-This would scan the logs for the 1st of Jan 2019 between 08:00h and 09:00h
+	.\ps_Util_GetEventLogByDateRange.ps1 -StartDate "01/01/2019 08:00:00" -EndDate "01/01/2019 09:00:00"
 
-.NOTES
-MVogwell - 16-05-19 - v1.1
+	This would scan the logs for the 1st of Jan 2019 between 08:00h and 09:00h
 
-v1.0 - Initial release
-v1.1 - Added event source (ProviderName) to the outputed data
+	.NOTES
+	MVogwell - 2022-11-24 - v1.2
 
-.LINK
+	v1.0 - Initial release
+	v1.1 - Added event source (ProviderName) to the outputed data
+	v1.2 - Removed Write-Host
+
+	.LINK
 
 #>
 
@@ -55,130 +58,195 @@ param (
 	[Parameter(Mandatory=$false)][string]$ExportFolderPath ="C:\temp\EventExport",
 	[Parameter(Mandatory=$false)][string]$StartDate = "",
 	[Parameter(Mandatory=$false)][string]$EndDate = "",
-	[Parameter(Mandatory=$false)][boolean]$ScanSecurityLog = $False
+	[Parameter(Mandatory=$false)][switch]$IncludeSecurityLog,
+	[Parameter(Mandatory=$false)][switch]$ExportIndividualLogs
 )
 
-Function ps_Function_CheckRunningAsAdmin {
-    [CmdletBinding()]
-    param()
-
-    # Constructor
-    [bool]$bRunningAsAdmin = $False
-
-    Try {
-        # Attempt to check if the current powershell session is being run with admin rights
-        # System.Security.Principal.WindowsIdentity -- https://msdn.microsoft.com/en-us/library/system.security.principal.windowsidentity(v=vs.110).aspx
-        # Info on Well Known Security Identifiers in Windows: https://support.microsoft.com/en-gb/help/243330/well-known-security-identifiers-in-windows-operating-systems
-
-        write-verbose "ps_Function_CheckRunningAsAdmin :: Checking for admin rights"
-        $bRunningAsAdmin = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
-    }
-    Catch {
-        $bRunningAsAdmin = $False
-        write-verbose "ps_Function_CheckRunningAsAdmin :: ERROR Checking for admin rights in current session"
-        write-verbose "ps_Function_CheckRunningAsAdmin :: Error: $($Error[0].Exception)"
-    }
-    Finally {}
-
-    write-verbose "ps_Function_CheckRunningAsAdmin :: Result :: $bRunningAsAdmin"
-
-    # Return result from function
-    return $bRunningAsAdmin
-
-}
-
-
 #@# Main
-write-host "Eventlog export by date range utility - MVogwell - 16-05-19 - v1.0 `n" -fore green
+Write-Output "`n`n=== Eventlog export by date range utility - MVogwell - Nov 2022 - v1.2 ===`n"
 
-$bRunningAsAdmin = ps_Function_CheckRunningAsAdmin
-if(!($bRunningAsAdmin)) {
-	write-host "You must be running as an admin to run this script. Please open an administrator elevated powershell session and try again! `n`n" -fore red #Exit
+try {
+	Write-Output "*** Checking if running as admin"
+
+	$bRunningAsAdmin = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
+
+	if ($bRunningAsAdmin -eq $true) {
+		Write-Output "`t+++ Session is running as admin `n"
+	}
+	else {
+		Write-Output "`t--- Session is NOT running as admin - not all logs (including Security) will be exported! `n"
+	}
 }
-Else {
-	If(!(Test-Path $ExportFolderPath)) {
-		$bCreateFolderFailed = $False
+catch {
+	Write-Output "`t--- Failed to discover whether this session is running as admin."
+	Write-Output "`t--- It is possible that not all logs will be exported. `n"
+}
 
+$bScanLog = $true	# flag to decide whether a log file should be scanned
+$bProceed = $true
+[System.Collections.ArrayList]$arrAllLogData = @()
+$sTimestamp = Get-Date -Format "yyyyMMdd-HHmmss"
+
+If((Test-Path $ExportFolderPath) -eq $false) {
+	Try {
+		Write-Output "*** Creating output folder $ExportFolderPath"
+
+		New-Item -Path $ExportFolderPath -ItemType Directory -Force | Out-Null
+
+		Write-Output "`t+++ Success"
+	}
+	Catch {
+		$bProceed = $false
+
+		Write-Output "Unable to create folder path $ExportFolderPath - the script will now exit! `n`n"
+	}
+}
+else {
+	Write-Output "*** Checking output folder $ExportFolderPath available"
+	Write-Output "`t+++ Success `n"
+}
+
+# Only continue if the results folder could be created
+If($bProceed -eq $true) {
+	$sComputerName = $env:ComputerName	# Get Current computer name
+
+	# Get a list of available Windows logs
+	try {
+		Write-Output "*** Enumerating available windows logs"
+
+		$arrEventLogNames = (Get-WinEvent -ComputerName $sComputerName -ListLog * -ea "SilentlyContinue").LogName
+
+		Write-Output "`t+++ Success `n"
+	}
+	catch {
+		$sErrMsg = ("Failed! Error: " + ($Error[0].Exception.Message).Replace("`r","").Replace("`n"," "))
+		Write-Output "`t---$sErrMsg"
+
+		$bProceed = $false
+	}
+
+# Set the start date to value in the param EventStartDate. If not available set it to 24 hours ago
+if ($bProceed -eq $true) {}
+	if ([string]::IsNullOrEmpty($StartDate)) {
+		$EventStartDate = ((Get-Date).AddDays(-1))
+	}
+	else {
+		try {
+			$EventStartDate = Get-Date($StartDate)
+		}
+		catch {
+			$bProceed = $false
+		}
+	}
+
+	# Set the end date to value in the param EventStartDate. If not available set it to 24 hours ago
+	If([string]::IsNullOrEmpty($EndDate)) {
+		$EventEndTime = Get-Date
+	}
+	Else {
 		Try {
-			New-Item -Path $ExportFolderPath -ItemType Directory -Force | Out-Null
+			$EventEndTime = Get-Date($EndDate)
 		}
 		Catch {
-			write-host "Unable to create folder path $ExportFolderPath - the script will now exit! `n`n" -fore red
-			$bCreateFolderFailed = $True
-		}
-	}
-
-	$arrAllLogData =@()
-
-	If(!($bCreateFolderFailed)) {
-		$ComputerName = $env:COMPUTERNAME#Current computer
-		$EventLogNames = (get-winevent -ComputerName $ComputerName -ListLog * -ea "SilentlyContinue").LogName
-
-		$bStartDateError =$False
-		If($StartDate.length -eq 0) {
-			$EventStartDate = ((Get-Date).AddDays(-1))
-		}
-		Else {
-			Try {
-				$EventStartDate = get-Date($StartDate)
-			}
-			Catch {
-				$bStartDateError = $True
-			}
-		}
-
-		$bEndDateError =$False
-		If($EndDate.length -eq 0) {
-			$EventEndTime = Get-Date
-		}
-		Else {
-			Try {
-				$EventEndTime = get-Date($EndDate)
-			}
-			Catch {
-				$bEndDateError = $True
-			}
-		}
-
-
-		write-host "Exporting log files to $ExportFolderPath" -fore yellow
-		write-host "Using delimiter: ~" -fore yellow
-		write-host "Scan Security Log?: $ScanSecurityLog" -fore yellow
-		write-host "Start Date: $($EventStartDate.toString())" -fore yellow
-		write-host "End Date: $($EventEndTime.toString()) `n" -fore yellow
-		write-verbose "Start date invalid: $bStartDateError "
-		write-verbose "End date invalid: $bEndDateError "
-
-		If(($bStartDateError -eq $True) -or($bEndDateError -eq $True)) {
-			write-host "Unable to set date!" -fore red # NEEDS WORK!
-		}
-		Else {
-			write-host "Exporting logs for... " -fore yellow
-			Foreach($EventLogName in $EventLogNames) {
-				$bScanLog = $True
-
-				If($EventLogName -eq "Security") {
-					If($ScanSecurityLog -eq $False) {
-						$bScanLog = $False
-					}
-				}
-
-				If($bScanLog -eq $True) {
-					write-host $EventLogName -fore yellow
-					$EventCritea = @{logname = $EventLogName; StartTime=$EventStartDate; EndTime=$EventEndTime}
-					$ExportPath = $ExportFolderPath +"\" + $EventLogName.replace("/","-").replace("\","-") + ".txt"
-					$Evts = Get-WinEvent -ComputerName $ComputerName -FilterHashTable $EventCritea  -ErrorAction SilentlyContinue | select LogName,MachineName,TimeCreated,ProviderName,Id,LevelDisplayName, @{n="Msg";e={$_.Message.replace("`n"," :: ").replace("`r"," :: ") }}
-					If(($Evts | Measure).count -gt 0) {
-						$Evts | Export-CSV $ExportPath -NoTypeInformation -Delimiter "~"
-						$arrAllLogData += $Evts
-					}
-				}
-			}
-
-			$ExportPath = $ExportFolderPath + "\_AllExportedLogData.txt"
-			$arrAllLogData | Export-CSV $ExportPath -NoTypeInformation -Delimiter "~"
-
-			write-host "`nAll events have been exported to $ExportPath - Please open the text files using Excel and select the delimiter ~`n`n" -fore green
+			$bProceed = $false
 		}
 	}
 }
+
+if ($bProceed -eq $false) {
+		Write-Output "Unable to set start or end date! Please check the values and try again"
+}
+else {
+	Write-Output "*** Startup parameters:"
+	Write-Output "`t... Computer name: $sComputerName"
+	Write-Output "`t... Exporting log files to $ExportFolderPath"
+	Write-Output "`t... Using delimiter: ~"
+	Write-Output "`t... Export Security Log?: $IncludeSecurityLog"
+	Write-Output "`t... Export individual log files: $ExportIndividualLogs"
+	Write-Output "`t... Start Date: $($EventStartDate.toString())"
+	Write-Output "`t... End Date: $($EventEndTime.toString()) `n"
+
+	Write-Output "*** Exporting logs:"
+
+	# Loop through each log returned from list all
+	Foreach($sEventLogName in $arrEventLogNames) {
+		try {
+			Write-Progress -Activity "Exporting log data" -Status "Logname: $sEventLogName"
+		}
+		catch {
+			Write-Progress -Activity "Exporting log data"
+		}
+
+		# Check if the current log is "Security. Only scan if startup parameter IncludeSecurityLog is false"
+		if (($sEventLogName -eq "Security") -and ($IncludeSecurityLog -eq $false)) {
+			$bScanLog = $false
+		}
+
+		If($bScanLog -eq $true) {
+			$hEventCritea = @{logname = $sEventLogName; StartTime=$EventStartDate; EndTime=$EventEndTime}
+
+			$sExportPath = $ExportFolderPath +"\" + $sEventLogName.replace("/","-").replace("\","-") + ".txt"
+
+			# Retrieve the log data
+			try {
+				$arrEvts = Get-WinEvent -ComputerName $sComputerName -FilterHashTable $hEventCritea  -ErrorAction SilentlyContinue | Select-Object LogName,MachineName,TimeCreated,ProviderName,Id,LevelDisplayName, @{n="Msg";e={$_.Message.replace("`n"," :: ").replace("`r","") }}
+			}
+			catch {
+				Write-Output "`t--- Error: $sEventLogName - unable to export log data!"
+				$arrEvts = @()
+			}
+
+			# Export the log data and add to the cumulative log file (arrAllLogData)
+			if ($null -eq $arrEvts) {
+				Write-Output "`t--- No data: $sEventLogName"
+			}
+			else {
+				if ($ExportIndividualLogs -eq $true) {
+					try {
+						$arrEvts | Sort-Object TimeCreated | Export-CSV $sExportPath -NoTypeInformation -Delimiter "~"
+					}
+					catch {
+						Write-Output "`t--- Failed to save individual log: $sExportPath"
+					}
+				}
+
+				# Add the retreived log events to the collation array
+				if ($arrEvts.GetType().BaseType.Name -eq 'Array') {
+					$arrAllLogData.AddRange($arrEvts)
+				}
+				else {
+					$arrAllLogData.Add($arrEvts)
+				}
+
+				Write-Output "`t+++ Completed: $sEventLogName"
+			}
+		}
+
+		# Tidy up - Clear the buffer of events
+		Remove-Variable arrEvts -ErrorAction "SilentlyContinue"
+
+		# Reset flag to scan logs
+		$bScanLog = $true
+	}
+
+	$sExportPath = $ExportFolderPath + "\_" + $sComputerName + "_AllExportedLogData_" + $sTimestamp + ".txt"
+	$arrAllLogData | Sort-Object TimeCreated,LogName | Export-CSV $sExportPath -NoTypeInformation -Delimiter "~"
+
+	# Display output information
+	if ($ExportIndividualLogs -eq $true) {
+		Write-Output "`nIndividual logs have been exported to $ExportFolderPath"
+	}
+
+	Write-Output "`n* Discovered log data from $($arrAllLogData.count) event logs"
+
+	Write-Output "* All events have been exported to $sExportPath"
+
+	Write-Output "* Please open the text files using Excel and select the delimiter ~`n"
+}
+
+
+# Tidy up
+Remove-Variable sErrMsg,sComputerName,bResultsFolderAvailable,ExportFolderPath,bProceed,sExportPath -ErrorAction "SilentlyContinue"
+Remove-Variable EventEndTime,EventStartDate,arrAllLogData,arrEventLogNames -ErrorAction "SilentlyContinue"
+
+Write-Output "=== Finished === `n`n"
